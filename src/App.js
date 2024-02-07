dd7531d8c04871b9327f525a359ae5c8ea8e4c19
Abongile Tshopi <abongiletshopi@gmail.com>
2024-02-07 14:37:09 +0200

    fixing my frame/containers for home

--- src/App.js
+++ src/App.js
@@ -4,47 +4,59 @@ import './App.css';
 function App() {
   return (
     <div className="mainFrame">
-      
 
+{/* <img className="logo" alt="logo" src="logo.png" /> */}
         <div className="nav-bar">
+        
+          <div className="nav-bar > div"> 
+                <div className="home-cage">
+                  <div className="text-wrapper">Home</div>
+                </div>
 
-          <div className="homeFrame">
-            
-          <div className="home-cage">
-            <div className="text-wrapper">Home</div>
-          </div>
+                <div className="about-cage">
+                  <div className="text-wrapper">About</div>
+                </div>
 
-          <div className="about-cage">
-            <div className="text-wrapper">About</div>
-          </div>
+                <div className="services-cage">
+                  <div className="text-wrapper">Services</div>
+                </div>
 
-          <div className="services-cage">
-            <div className="text-wrapper">Services</div>
-          </div>
-
-          <div className="project-cage">
-            <div className="text-wrapper">Projects</div>
-          </div>
-
-
-          <div className="contact-cage">
-            <div className="text-wrapper">Contact</div>
-          </div>
-          
-          
+                <div className="project-cage">
+                  <div className="text-wrapper">Projects</div>
+                </div>
 
 
-          
-          
+                <div className="contact-cage">
+                  <div className="text-wrapper">Contact</div>
+                </div>
+          </div> 
+        </div> 
+
+
+        
+        
+
+        <div className="removebg-wrapper">
           
         </div>
-
         
+        <div className="text-hi">Hi!</div>
+        <div className="itsMe">it’s me</div>
+        <div className='text-wrapper'>Abongile  Tshopi</div>
+        <div className="and">And I am a</div> 
+        <div className='text-wrapper'>Junior Software Developer</div>
         
 
+        <p className="enthusiastic-junior">
+          Enthusiastic Junior Software Developer with a dedication to creating quality software solutions <br />
+          and delivering impactful work across the development stack.
+        </p>
+        
+        <img className="mountyns" alt="Mountyns" src="mountyns-1.png" />
 
 
-        </div>  
+
+         
     </div>
   );
 }

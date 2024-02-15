70a51aa8a7b510492a4b55781f0030410f68e805
Abongile Tshopi <abongiletshopi@gmail.com>
2024-02-15 11:40:37 +0200

    imported images

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,73 @@
 import "./Projects.css";
 import React from "react";
 
-function template() {
+function projects() {
   return (
-    <div className="projects">
-      <h1>Projects</h1>
-    </div>
-  );
-};
+    <div className="mainAppFrame">
+      <div className="mainFrame">
+        <div className="nav-bar">
+          <img className="logo" alt="logo" src="logo.png" />
+          <div className="menu-cage">
+            <div className="home-cage">
+              <div className="text-wrapper">Home</div>
+            </div>
 
-export default template;
+            <div className="about-cage">
+              <div className="text-wrapper">About</div>
+            </div>
+
+            <div className="services-cage">
+              <div className="text-wrapper">Services</div>
+            </div>
+
+            <div className="projects-cage">
+              <div className="text-wrapper">Projects</div>
+            </div>
+
+            <div className="contact-cage">
+              <div className="text-wrapper">Contact</div>
+            </div>
+          </div>
+        </div>
+{/* ------------------my Projects page-------------------------------- */}
+<div className="contactText"><span>My </span>Projects</div>
+    
+<div className="projectscontainer">
+
+        <div className="bankAppBox">
+        </div>
+
+
+        <div className="sneshAppBox">  
+        </div>
+
+
+        <div className="financeAppBox">
+        </div>
+
+
+        <div className="projectscontainer">
+        </div>
+        
+</div>
+
+
+
+
+
+
+{/*---------------- social media button icons----------------  */}
+        <div className="projectsImage">
+          <img className="" src="" />
+        </div>
+
+        {/* <button type="submit">Submit</button> */}
+
+
+
+  {/* closinf the main frames */}
+</div>  
+</div>
+  );
+}
+export default projects;

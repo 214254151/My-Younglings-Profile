241786a121f808efc9f3e396e31a7984256ef360
Abongile Tshopi <abongiletshopi@gmail.com>
2024-02-16 15:53:57 +0200

    fixed hovering of projects boxes

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -51,23 +51,25 @@ function projects() {
                                                   Seamlessly manage your accounts, transfer funds effortlessly, 
                                                   and stay in control of your finances on the go">
                 <span></span>
-
                 <div className="bankName">BankApp</div>
-
             </div>
 
 
-            <div className="sneshAppBox">
-            <span></span>
-            <dive className="sneshName">BakeryApp</dive>
+            <div className="sneshAppBox" data-info ="With easy customization options and a user-friendly interface, 
+                                                    our bakery app is your gateway to a world of sweetness and simplicity. 
+                                                    Elevate your pastry experience . It's time to treat yourself!">
+              <span></span>
+              <dive className="sneshName">BakeryApp</dive>
             </div>
 
-            <div className="financeAppBox">
-            <span></span>
-            <div className="financeName">FinanceApp</div>
+            <div className="financeAppBox" data-info="Enjoy precision and convenience with features like 
+                                                      customizable investment projections, 
+                                                      loan amortization schedules, and real-time currency conversion.">
+              <span></span>
+              <div className="financeName">FinanceApp</div>
             </div>
 
-            <div className="projectscontainer"></div>
+            
           </div>
         </div>
 

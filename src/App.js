303ff52a5d1aa37af640b98833578f43e69f70dc
Abongile Tshopi <abongiletshopi@gmail.com>
2024-02-19 10:14:46 +0200

    spaces

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ import Services from "./components/Services/Services";
 import Contact from "./components/Contact/Contact";
 import Projects from "./components/Projects/Projects";
 
-
-
-
 function App() {
   return (
      <Router>
